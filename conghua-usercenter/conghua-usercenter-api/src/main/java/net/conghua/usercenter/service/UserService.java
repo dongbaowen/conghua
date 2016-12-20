0a1a30f50094d87cb7e701cc4fd4e6335310ee4c
dongbaowen <dongbaowen92@126.com>
2016-12-21 00:25:16 +0800

    create

---
+++ conghua-usercenter/conghua-usercenter-api/src/main/java/net/conghua/usercenter/service/UserService.java
@@ -0,0 +1,17 @@
+package net.conghua.usercenter.service;
+
+import net.conghua.usercenter.beans.User;
+
+/**
+ * Created by Baowen on 2016/12/20.
+ */
+public class UserService {
+
+    public User queryUser() {
+
+
+
+        return null;
+    }
+
+}
---
+++ conghua-usercenter/conghua-usercenter-bean/src/main/java/net/conghua/usercenter/beans/User.java
@@ -0,0 +1,55 @@
+package net.conghua.usercenter.beans;
+
+import java.io.Serializable;
+
+/**
+ * Created by Baowen on 2016/12/20.
+ */
+public class User implements Serializable{
+
+    private long id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
